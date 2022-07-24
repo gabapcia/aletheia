fd66e9d956b21c55dbb1697d070ab6fe1c37714e
Gabriel <gabrielbgarcia0999@gmail.com>
2022-07-24 19:42:31 -0300

    chore: plugins updated

--- plugins/minio_plugin/utils/file.py
+++ plugins/minio_plugin/utils/file.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import re
 import httpx
 from httpx import codes
 
@@ -26,10 +27,11 @@ class FileReader(ABC):
 
 
 class HTTPFile(FileReader):
-    def __init__(self, uri: str, name: str = '', timeout: int = 5) -> None:
+    def __init__(self, uri: str, name: str = '', timeout: int = 5, **attrs) -> None:
         self._uri = uri
         self._name = name
         self._timeout = timeout
+        self._attrs = attrs
 
     def __enter__(self) -> 'HTTPFile':
         self._stream = httpx.stream('GET', self._uri, timeout=self._timeout)
@@ -61,13 +63,27 @@ class HTTPFile(FileReader):
         return buffer
 
     def content_type(self) -> str:
+        ct = self._attrs.get('content_type', None)
+        if ct:
+            return ct
+
         ct = self._response.headers['Content-Type']
         return ct
 
     def size(self) -> int:
-        co = int(self._response.headers.get('Content-Length', -1))
-        return co
+        cl = self._attrs.get('size', None)
+        if cl:
+            return cl
+
+        cl = int(self._response.headers.get('Content-Length', -1))
+        return cl
 
     def name(self) -> str:
+        filename_re = re.search(r'filename="(?P<data>[^"])"', self._response.headers.get('Content-Disposition', ''))
+
+        if filename_re:
+            name = filename_re.group('data')
+            return name
+
         name = self._name or self._uri.split('/')[-1].split('?')[0]
         return name
